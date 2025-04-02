b248dadcd38f2ea513543cf3b2614307d622c2b9
Joe Roberts <joe@engiqueer.net>
2025-04-02 18:17:37 +0100

    Fix eleventy config

--- .eleventy.mjs
+++ .eleventy.mjs
@@ -1,4 +1,5 @@
 export default function (eleventyConfig) {
   // Order matters, put this at the top of your configuration file.
   eleventyConfig.setInputDirectory("content");
+  eleventyConfig.addPassthroughCopy({ "content/admin/assets": "admin/assets" });
 }
